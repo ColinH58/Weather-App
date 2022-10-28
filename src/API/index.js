d7b78b6089f89a9b8a4505ac4130344d8cfd80ab
ColinH58 <choolwerf@gmail.com>
2022-10-27 19:41:25 -0500

    changed APIs

--- src/API/index.js
+++ src/API/index.js
@@ -1,44 +1,14 @@
-export const BASE_URL = 'https://climacell-microweather-v1.p.rapidapi.com'
-export const KEY = 'f1077fc348mshb3be5982aabc937p1a38a7jsn5df5d5db6842'
+export const BASE_URL = 'http://api.openweathermap.org'
+export const KEY = '0cce63ace2ee37502397fdf44a8d86a2'
 
-// options? not sure what this does?
-export const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': `${KEY}`,
-		'X-RapidAPI-Host': 'climacell-microweather-v1.p.rapidapi.com'
-	}
+// Get the CURRENT weather
+export const getCurrentWeather = async (lat, lon) => {
+    const response = await fetch(`${BASE_URL}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${KEY}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    })
+    const data = await response.json();
+    return data
 };
-
-// Short Term Forecast (Nowcast)
-export async function getForecast(lat, lon, fields) {
-    try {
-        const response = await fetch(`${BASE_URL}/weather/nowcast?lon=${lon}&lat=${lat}&fields=${fields}`, options)
-        const data = await response.json();
-        return data
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-// Hourly Forecast
-export async function getHourlyForecast(lat, lon, fields) {
-    try {
-        const response = await fetch(`${BASE_URL}/weather/forecast/hourly?lat=${lat}&lon=${lon}&fields=${fields}`, options)
-        const data = await response.json();
-        return data
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-// Realtime
-export async function getCurrentWeather(lat, lon, fields) {
-    try {
-        const response = await fetch(`${BASE_URL}/weather/realtime?lat=${lat}&lon=${lon}&fields=${fields}`, options)
-        const data = await response.json();
-        return data
-    } catch (error) {
-        console.log(error)
-    }
-}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Home } from "./Components";
+// import { getCurrentWeather } from "./API"
 
 function App() {
+// const [test, setTest] = useState({})
+
+
   return (
     <div className="App">
       <Home />
+      {/* <h1>{test}</h1> */}
     </div>
   );
 }
