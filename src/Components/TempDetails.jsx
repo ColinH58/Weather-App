f70436a2b1608f67828e63cbba19f6b3fa73ba17
ColinH58 <choolwerf@gmail.com>
2022-10-28 05:53:08 -0500

    making progress

--- src/Components/TempDetails.jsx
+++ src/Components/TempDetails.jsx
@@ -1,14 +1,43 @@
 import React from "react";
-import { UilSnowflake } from "@iconscout/react-unicons";
+import {
+  UilArrowUp,
+  UilArrowDown,
+  UilTemperature,
+  UilTear,
+  UilWind,
+  UilSun,
+  UilSunset,
+} from "@iconscout/react-unicons";
 
 const TempDetails = () => {
   return (
     <div className="TempDetails">
       <div className="CurrentConditionLabel">Snowy</div>
+
       <div className="CurrentConditionReadOut">
-        <UilSnowflake />
-        <p>25°</p>
-        <p>READ OUT IMAGES</p>
+        <img
+          className="ConditionIcon"
+          src="https://image.shutterstock.com/image-vector/simple-black-snowflake-rounded-corners-600w-1500901745.jpg"
+          alt="temp snowflake icon"
+        />
+        <p className="Temperature">25°</p>
+        <div className="ReadOutBlock">
+          <div>
+            <UilTemperature />
+            Feel's Like:
+            <span className="ReadOutInfo">23°</span>
+          </div>
+          <div>
+            <UilTear />
+            Humidity:
+            <span className="ReadOutInfo">23°</span>
+          </div>
+          <div>
+            <UilWind />
+            Wind:
+            <span className="ReadOutInfo">12 km/h</span>
+          </div>
+        </div>
       </div>
     </div>
   );
