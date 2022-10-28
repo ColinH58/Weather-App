4bc7e0a18dccfbbff168afb3aaa3bd7ea74aa630
ColinH58 <choolwerf@gmail.com>
2022-10-27 20:26:08 -0500

    finishing for today

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Home } from "./Components";
-// import { getCurrentWeather } from "./API"
 
 function App() {
-// const [test, setTest] = useState({})
-
 
   return (
     <div className="App">
       <Home />
-      {/* <h1>{test}</h1> */}
     </div>
   );
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Search, Forecast, Loading, Error } from "./index"
+import { getCurrentWeather } from "../API"
 
 const Home = () => {
+const currentWeather = getCurrentWeather()
+
   return (
     <div>
         <Search />
         <Forecast />
+        <h1>currentWeather</h1>
         {/* {isLoading && <Loading />} */}
         {/* {isError && <Error />} */}
     </div>
