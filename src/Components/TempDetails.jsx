0566ae77cc3f0039ae192b4c91e79c034498e2f3
ColinH58 <choolwerf@gmail.com>
2022-10-28 06:52:23 -0500

    stopping for now

--- src/Components/TempDetails.jsx
+++ src/Components/TempDetails.jsx
@@ -19,38 +19,39 @@ const TempDetails = () => {
           src="https://image.shutterstock.com/image-vector/simple-black-snowflake-rounded-corners-600w-1500901745.jpg"
           alt="temp snowflake icon"
         />
-        {/* Large Temperature Read Out in the Middle */}
-        <p className="Temperature">25°</p>
+        {/* Large Temperature Read Out with High and Low in the Middle */}
+        <div>
+          <p className="Temperature">25°</p>
+          <div className="HighLow">
+            <div className="ReadOutLine">
+              <UilArrowUp className="ReadOutIcon" />
+              High:
+              <span className="ReadOutInfo">28°</span>
+            </div>
+            <div className="ReadOutLine">
+              <UilArrowDown className="ReadOutIcon" />
+              Low:
+              <span className="ReadOutInfo">19°</span>
+            </div>
+          </div>
+        </div>
         {/* Small Weather Information Block on the Right */}
         <div className="ReadOutBlock">
-          <div>
+          <div className="ReadOutLine">
             <UilTemperature className="ReadOutIcon" />
             Feel's Like:
             <span className="ReadOutInfo">23°</span>
           </div>
-          <div>
+          <div className="ReadOutLine">
             <UilTear className="ReadOutIcon" />
             Humidity:
             <span className="ReadOutInfo">23°</span>
           </div>
-          <div>
+          <div className="ReadOutLine">
             <UilWind className="ReadOutIcon" />
             Wind:
             <span className="ReadOutInfo">12 km/h</span>
           </div>
-        </div>
-      </div>
-      {/* High and Low Information at the Bottom */}
-      <div className="HighLow">
-        <div>
-          <UilArrowUp className="HighLowIcon" />
-          High:
-          <span className="ReadOutInfo">28°</span>
-        </div>
-        <div>
-          <UilArrowDown className="HighLowIcon" />
-          Low:
-          <span className="ReadOutInfo">19°</span>
         </div>
       </div>
     </div>
