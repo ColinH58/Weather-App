84c48c256d257aa1d64eb583ac12e88af5772cdb
ColinH58 <choolwerf@gmail.com>
2022-11-10 21:43:53 -0600

    cleaned up code a bit, stopping for now

--- src/API/WeatherServicesApi.js
+++ src/API/WeatherServicesApi.js
@@ -1,18 +1,6 @@
 const BASE_URL = "https://api.openweathermap.org/data/2.5/";
 const KEY = "0cce63ace2ee37502397fdf44a8d86a2";
 
-// Get the CURRENT weather
-// const getCurrentWeather = async (city) => {
-//     const response = await fetch(`${BASE_URL}/weather?q=${city}&appid=${KEY}`, {
-//         method: "GET",
-//         headers: {
-//             "Content-Type": "application/json"
-//         }
-//     })
-//     const data = await response.json();
-//     return data
-// };
-
 const getWeatherData = (infoType, searchParams) => {
   const url = new URL(BASE_URL + "/" + infoType);
   url.search = new URLSearchParams({ ...searchParams, appid: KEY });
@@ -30,15 +18,12 @@ const formatCurrentWeather = (data) => {
     weather,
     wind: {speed}
   } = data
-
   const {main: details, icon} = weather[0]
-
   return {lat, lon, temp, feels_like, temp_min, temp_max, humidity, name, dt, country, sunrise, sunset, details, icon, speed}
 }
 
 const getFormattedWeatherData = async (searchParams) => {
   const formattedCurrentWeather = await getWeatherData("weather", searchParams).then(formatCurrentWeather)
-
   return formattedCurrentWeather
 }
 
