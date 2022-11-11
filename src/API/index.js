b8269081342d3a3558116cfca6056286255a1b7f
ColinH58 <choolwerf@gmail.com>
2022-11-10 21:08:27 -0600

    working on the API calls now

--- src/API/index.js
+++ src/API/index.js
@@ -1,9 +1,9 @@
-export const BASE_URL = 'http://api.openweathermap.org'
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5'
 export const KEY = '0cce63ace2ee37502397fdf44a8d86a2'
 
 // Get the CURRENT weather
-export const getCurrentWeather = async (lat, lon) => {
-    const response = await fetch(`${BASE_URL}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${KEY}`, {
+export const getCurrentWeather = async (city, key) => {
+    const response = await fetch(`${BASE_URL}/weather?q=${city}&appid=${KEY}`, {
         method: "GET",
         headers: {
             "Content-Type": "application/json"
