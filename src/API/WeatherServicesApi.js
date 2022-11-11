cf83052970822ce32be8a72d50881e93a9daf2a7
ColinH58 <choolwerf@gmail.com>
2022-11-10 23:27:32 -0600

    added Luxon and uncommented more functions

--- src/API/WeatherServicesApi.js
+++ src/API/WeatherServicesApi.js
@@ -39,26 +39,26 @@ const formatCurrentWeather = (data) => {
   };
 };
 
-// const formatForecastWeather = (data) => {
-//   let { timezone, daily, hourly } = data;
-//   daily = daily.slice(1, 6).map((d) => {
-//     return {
-//       title: formatToLocalTime(d.dt, timezone, "ccc"),
-//       temp: d.temp.day,
-//       icon: d.weather[0].icon,
-//     };
-//   });
+const formatForecastWeather = (data) => {
+  let { timezone, daily, hourly } = data;
+  daily = daily.slice(1, 6).map((d) => {
+    return {
+      title: formatToLocalTime(d.dt, timezone, "ccc"),
+      temp: d.temp.day,
+      icon: d.weather[0].icon,
+    };
+  });
 
-//   hourly = hourly.slice(1, 6).map((d) => {
-//     return {
-//       title: formatToLocalTime(d.dt, timezone, "hh:mm a"),
-//       temp: d.temp,
-//       icon: d.weather[0].icon,
-//     };
-//   });
+  hourly = hourly.slice(1, 6).map((d) => {
+    return {
+      title: formatToLocalTime(d.dt, timezone, "hh:mm a"),
+      temp: d.temp,
+      icon: d.weather[0].icon,
+    };
+  });
 
-//   return { timezone, daily, hourly };
-// };
+  return { timezone, daily, hourly };
+};
 
 const getFormattedWeatherData = async (searchParams) => {
   const formattedCurrentWeather = await getWeatherData(
